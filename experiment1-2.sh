895f0800d7c5bf8f2e07bf73f01deb1e77dc3ee3
Christie <christie@ziggo.nl>
2021-03-29 14:36:39 +0200

    full experiment

---
+++ experiment1-2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#SBATCH --job-name=experiment1_full
+#SBATCH --partition=cpu-short
+#SBATCH -c 1
+#SBATCH --time=0:30:00
+#SBATCH --mem-per-cpu=16gb
+
+export PYTHONPATH=/home/s2155435/bep1/
+module load Miniconda3/4.7.10
+conda init bash
+source ~/.bashrc
+conda activate bepalice
+python3 /home/s2155435/bep1/pandafy_data/scriptsSerialized.py n 1 &>> /home/s2155435/terminaloutput2.txt
