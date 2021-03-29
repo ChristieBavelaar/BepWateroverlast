d100e6d236168bfdaf31f5fe1846233d79150c43
Christie <christie@ziggo.nl>
2021-03-29 14:27:26 +0200

    redirect terminal output

--- experiment1.sh
+++ experiment1.sh
@@ -10,4 +10,4 @@ module load Miniconda3/4.7.10
 conda init bash
 source ~/.bashrc
 conda activate bepalice
-python3 home/s2155435/bep1/pandafy_data/scriptsSerialized.py y 1
+python3 /home/s2155435/bep1/pandafy_data/scriptsSerialized.py y 1 &>> /home/s2155435/terminaloutput1.txt
