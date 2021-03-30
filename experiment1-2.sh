bfc116908867cbb9ec81cd48afdba9ae47c8b42d
Christie <christie@ziggo.nl>
2021-03-30 11:12:41 +0200

    experiment fix

--- experiment1-2.sh
+++ experiment1-2.sh
@@ -10,4 +10,4 @@ module load Miniconda3/4.7.10
 conda init bash
 source ~/.bashrc
 conda activate bepalice
-python3 /home/s2155435/bep1/pandafy_data/scriptsSerialized.py n 1 &>> /home/s2155435/terminaloutput2.txt
+python3 /home/s2155435/bep1/pandafy_data/scriptsSerialized.py n 0 &>> /home/s2155435/terminaloutput2.txt
