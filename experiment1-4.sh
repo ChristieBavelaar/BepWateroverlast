67e6d4eac2b269ea906ce1fdad837020ab43b9f2
Christie <christie@ziggo.nl>
2021-04-01 09:16:27 +0200

    new experiment

--- experiment1-4.sh
+++ experiment1-4.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#SBATCH --job-name=ex1-3
-#SBATCH --output=/home/s2155435/ex1-3.out
-#SBATCH --error=/home/s2155435/ex1-3.err
+#SBATCH --job-name=ex1-4
+#SBATCH --output=/home/s2155435/ex1-4.out
+#SBATCH --error=/home/s2155435/ex1-4.err
 #SBATCH --mail-user="christie@ziggo.nl"
 #SBATCH --mail-type="ALL"
 #SBATCH --partition=cpu-medium
 #SBATCH -c 2
-#SBATCH --time=1-00:00:00
+#SBATCH --time=05:00:00
 #SBATCH --mem-per-cpu=30gb
 
 export PYTHONPATH=/home/s2155435/bep1/
@@ -14,4 +14,4 @@ module load Miniconda3/4.7.10
 conda init bash
 source ~/.bashrc
 conda activate bepalice
-python3 /home/s2155435/bep1/pandafy_data/scriptsSerialized.py n 0 > /home/s2155435/terminaloutput3.txt
+python3 /home/s2155435/bep1/pandafy_data/scriptsSerialized.py n 8 > /home/s2155435/terminaloutput3.txt
