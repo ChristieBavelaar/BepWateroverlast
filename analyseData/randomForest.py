8bacc823d1935232257af953ad1dbb06cdcb6efb
Christie <christie@ziggo.nl>
2021-04-02 17:55:40 +0200

    performance measures

--- analyseData/randomForest.py
+++ analyseData/randomForest.py
@@ -3,16 +3,18 @@ import numpy as np
 import sys
 import os
 from numpy import savetxt
+import matplotlib.pyplot as plt
 #sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import tree
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, precision_score, recall_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 
 def randomForest(folder='/home/s2155435/pandafied_data/', inputFile='finalData.csv'):
-    resultFile = open ("/home/s2155435/bep1/analyseData/results/resultRFAlice.txt", "w+")
+    resultFolder = '/home/s2155435/bep1/analyseData/results/'
+    resultFile = open (resultFolder+"resultRFAlice.txt", "w+")
     #load data
     rainTweets_eq = pd.read_csv(folder + inputFile)
     print("data loaded")
@@ -43,6 +45,10 @@ def randomForest(folder='/home/s2155435/pandafied_data/', inputFile='finalData.c
     skf = StratifiedKFold(n_splits=10)
     mape = []
     treeNumber = 0
+    accuracyResult = []
+    precisionResult = []
+    recallResult = []
+    totalConfusion = [[0,0],[0,0]]
     for train_index, test_index in skf.split(features, labels):
         #print("Train: ", train_index, " Test: ", test_index)
         train_features, test_features = features[train_index], features[test_index]
@@ -57,26 +63,48 @@ def randomForest(folder='/home/s2155435/pandafied_data/', inputFile='finalData.c
         #errors = abs(label_prediction - test_labels)
         #print('Mean Absolute Error:', round(np.mean(errors), 2))
         # Pull out one tree from the forest
-        resultFile.write("Accuracy:")
-        resultFile.write(str(metrics.accuracy_score(test_labels, label_prediction))+'\n')
-        resultFile.write(str(confusion_matrix(test_labels,label_prediction))+'\n\n')
+        confusion = confusion_matrix(test_labels,label_prediction)
+        totalConfusion[0][0] += confusion[0][0]
+        totalConfusion[0][1] += confusion[0][1]
+        totalConfusion[1][0] += confusion[1][0]
+        totalConfusion[1][1] += confusion[1][1]
+
+        accuracyResult.append(metrics.accuracy_score(test_labels, label_prediction))
+        precisionResult.append(precision_score(test_labels, label_prediction))
+        recallResult.append(recall_score(test_labels, label_prediction))
+
+        resultFile.write("Fold "+str(treeNumber)+"\n")
+        resultFile.write(str(confusion)+'\n')
+        resultFile.write("Accuracy: "+str(metrics.accuracy_score(test_labels, label_prediction))+"\n")
+        resultFile.write("Precision: "+str(precision_score(test_labels, label_prediction))+"\n")
+        resultFile.write("Recall: "+str(recall_score(test_labels, label_prediction)) + "\n\n")
         
         tree = rf.estimators_[4]# Import tools needed for visualization
         from sklearn.tree import export_graphviz
         import pydot# Pull out one tree from the forest
         tree = rf.estimators_[5]# Export the image to a dot file
-        outputFile = "/home/s2155435/bep1/analyseData/results/tree"+str(treeNumber)+".dot"
+        outputFile = resultFolder+"tree"+str(treeNumber)+".dot"
         export_graphviz(tree, out_file = outputFile, feature_names = feature_list, rounded = True, precision = 1)# Use dot file to create a graph
         #(graph, ) = pydot.graph_from_dot_file('tree.dot')# Write graph to a png file
         #graph.write_png('tree.png')
         treeNumber+=1
         
     #output cross validation performance
-    #all_accuracies = cross_val_score(estimator=rf, X=features, y=labels, cv=9)
+    #all_accuracies = cross_val_score(estimator=rf, X=features, y=labels, cv=10)
 
-    #resultFile.write(all_accuracies)
-    #resultFile.close()
+    fig, ax = plt.subplots()
+    data = [accuracyResult, precisionResult, recallResult]
+    xlabels = ["Accuracy", "Precision", "Recall"]
+    ax.boxplot(data)
+    ax.set_xticklabels(xlabels)
+    resultFile.write("\nAverage accuracy: "+str(np.average(accuracyResult))+"\n")
+    resultFile.write("Average Precision: "+str(np.average(precisionResult))+"\n")
+    resultFile.write("Average Recall: "+ str(np.average(recallResult))+"\n")
+    resultFile.write("Total Confusion matrix: \n["+str(totalConfusion[0][0])+","+ str(totalConfusion[0][1])+"] \n"+"["+str(totalConfusion[1][0])+","+ str(totalConfusion[1][1])+"] \n")
+    resultFile.close()
     #print(cross_val_score(estimator=rf, X=features, y=labels, cv=skf, scoring="accuracy"))
+    plt.savefig(resultFolder+"boxplotMeasures.png")
+    plt.show()
    
 if __name__ == '__main__':
     
