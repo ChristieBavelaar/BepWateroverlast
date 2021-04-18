ee365ca7e21454cd15a5a2fd35b9d10045257588
Christie <christie@ziggo.nl>
2021-04-18 12:24:35 +0200

    bugfix

--- analyseData/randomForest.py
+++ analyseData/randomForest.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 
 def randomForest(folder='/data/s2155435/pandafied_data/', inputFile='finalData.csv'):
+    print("open: ",inputFile)
     #resultFolder = '/home/s2155435/bep1/analyseData/results/'
     resultFolder = './'
     resultFile = open (resultFolder+"resultRFAlice.txt", "w+")
