fbe0b607aca7a6380de34c42d8b897349c2263db
Christie <christie@ziggo.nl>
2021-04-19 15:14:55 +0200

    filepath

--- analyse112/rfDepSamp.py
+++ analyse112/rfDepSamp.py
@@ -13,8 +13,8 @@ from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 
 def randomForest(folder='/data/s2155435/csv112/', inputFile='finalData.csv'):
-    #resultFolder = '/home/s2155435/bep1/experiments/scripts/'
-    resultFolder = './results/'
+    resultFolder = '/home/s2155435/bep1/experiments/scripts/'
+    #resultFolder = './results/'
     resultFile = open (resultFolder+"resultRFAlice.txt", "w+")
     
     #load data
