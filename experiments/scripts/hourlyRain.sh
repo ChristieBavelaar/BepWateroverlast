37bbba4273d7c596cb972dce7ec2496cb0185fc9
Christie <christie@ziggo.nl>
2021-05-05 18:15:59 +0200

    bugfix

--- experiments/scripts/hourlyRain.sh
+++ experiments/scripts/hourlyRain.sh
@@ -5,8 +5,8 @@
 #SBATCH --mail-user="christie@ziggo.nl"
 #SBATCH --mail-type="ALL"
 #SBATCH --partition=cpu-medium
-#SBATCH -c 8
-#SBATCH --time=1-00:00:00
+#SBATCH -c 1
+#SBATCH --time=10:00:00
 #SBATCH --mem-per-cpu=32gb
 
 export PYTHONPATH=/home/s2155435/bep1/
--- preprocess112/sampleNeg.py
+++ preprocess112/sampleNeg.py
@@ -94,24 +94,26 @@ def dependent_sampling(pos_data, neg_data, saveFile):
 
     # Add in the first sample
     # Choose a sample from a dataframe with negative examples having the same radarX and radarY
-    temp = neg_data.loc[(neg_data['radarX'] == pos_data.iloc[0]['radarX']) & (neg_data['radarY'] == pos_data.iloc[0]['radarY']) ]
-    negEq = negEq.append(temp.sample())
-
     for i in range(1,len(pos_data.index)):
-        temp = neg_data.loc[(neg_data['radarX'] == pos_data.iloc[i]['radarX']) & (neg_data['radarY'] == pos_data.iloc[i]['radarY']) ]
-        newRow = temp.sample()
-
-        # When to positive examples in the same radar space occur it is possible to add in a duplicate. Check that the sample is not allready contained in negEq
-        while not negEq.loc[(negEq['radarX'] == newRow.iloc[0]['radarX']) & (negEq['radarY'] == newRow.iloc[0]['radarY']) & (negEq['date'] == newRow.iloc[0]['date'])].empty:
+        try:
+            temp = neg_data.loc[(neg_data['radarX'] == pos_data.iloc[i]['radarX']) & (neg_data['radarY'] == pos_data.iloc[i]['radarY']) ]
             newRow = temp.sample()
-        negEq = negEq.append(newRow)
+                # When to positive examples in the same radar space occur it is possible to add in a duplicate. Check that the sample is not allready contained in negEq
+                while not negEq.loc[(negEq['radarX'] == newRow.iloc[0]['radarX']) & (negEq['radarY'] == newRow.iloc[0]['radarY']) & (negEq['date'] == newRow.iloc[0]['date'])].empty:
+                    newRow = temp.sample()
+                negEq = negEq.append(newRow)
+        except:
+            print("radarX",pos_data.iloc[i]['radarX'])
+            print("radarY",pos_data.iloc[i]['radarY'])
+            print(neg_data.loc[(neg_data['radarX'] == pos_data.iloc[i]['radarX']) & (neg_data['radarY'] == pos_data.iloc[i]['radarY']) ])
 
-    # Add the same coordinates to the negative example as the positive examples
+
+        # Add the same coordinates to the negative example as the positive examples
     negEq = negEq.reset_index(drop=True)
     pos_data = pos_data.reset_index(drop=True)
     negEq['latitude'] = pos_data['latitude']
     negEq['longitude'] = pos_data['longitude']
-
+    
     # Save to file
     data = pd.concat([pos_data, negEq], ignore_index=True)
     data.to_csv(saveFile, index=False)
