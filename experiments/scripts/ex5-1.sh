801d937dbb16b3c30cc0f2b7b566cf69d556fd44
Christie <christie@ziggo.nl>
2021-05-19 09:02:14 +0200

    more resources

--- experiments/scripts/ex5-1.sh
+++ experiments/scripts/ex5-1.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-type="ALL"
 #SBATCH --partition=cpu-short
 #SBATCH -c 1
-#SBATCH --time=01:00:00
+#SBATCH --time=03:00:00
 #SBATCH --mem-per-cpu=16gb
 
 export PYTHONPATH=/home/s2155435/bep1/
