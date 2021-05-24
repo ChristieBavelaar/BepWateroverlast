6c3240b07ca894cbe3e1287483b63b4362b33c9c
Christie <christie@ziggo.nl>
2021-05-24 17:06:27 +0200

    more memory

--- experiments/scripts/rain.sh
+++ experiments/scripts/rain.sh
@@ -4,10 +4,10 @@
 #SBATCH --error=/home/s2155435/rain.err
 #SBATCH --mail-user="christie@ziggo.nl"
 #SBATCH --mail-type="ALL"
-#SBATCH --partition=cpu-medium
+#SBATCH --partition=cpu-long
 #SBATCH -c 2
-#SBATCH --time=1-00:00:00
-#SBATCH --mem-per-cpu=32gb
+#SBATCH --time=2-00:00:00
+#SBATCH --mem-per-cpu=64gb
 
 export PYTHONPATH=/home/s2155435/bep1/
 module load Miniconda3/4.7.10
