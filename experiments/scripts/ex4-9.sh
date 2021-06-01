3a56c95566dea437a828dc0198b6f336293dd7d3
Christie <christie@ziggo.nl>
2021-06-01 13:40:14 +0200

    depsampdataset

--- experiments/scripts/ex4-9.sh
+++ experiments/scripts/ex4-9.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #SBATCH --job-name=ex4
-#SBATCH --output=/home/s2155435/ex4.out
-#SBATCH --error=/home/s2155435/ex4.err
+#SBATCH --output=/home/s2155435/ex4-9.out
+#SBATCH --error=/home/s2155435/ex4-9.err
 #SBATCH --mail-user="christie@ziggo.nl"
 #SBATCH --mail-type="ALL"
 #SBATCH --partition=cpu-long
-#SBATCH -c 2
+#SBATCH -c 1
 #SBATCH --time=1-00:00:00
 #SBATCH --mem-per-cpu=32gb
 
@@ -14,4 +14,4 @@ module load Miniconda3/4.7.10
 conda init bash
 source ~/.bashrc
 conda activate bepalice
-python3 /home/s2155435/bep1/preprocess112/serialized.py n 10 2 5
+python3 /home/s2155435/bep1/preprocess112/serialized.py n 10 3 7
