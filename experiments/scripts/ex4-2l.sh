ccc57ccf2e45ffbdb9b031cf10febf0b0aa30577
Christie <christie@ziggo.nl>
2021-06-03 15:39:57 +0200

    experiments on long partition

---
+++ experiments/scripts/ex4-2l.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --job-name=ex4-2l
+#SBATCH --output=/home/s2155435/ex4-2l.out
+#SBATCH --error=/home/s2155435/ex4-2l.err
+#SBATCH --mail-user="christie@ziggo.nl"
+#SBATCH --mail-type="ALL"
+#SBATCH --partition=cpu-medium
+#SBATCH -c 1
+#SBATCH --time=1-00:00:00
+#SBATCH --mem-per-cpu=16gb
+
+export PYTHONPATH=/home/s2155435/bep1/
+module load Miniconda3/4.7.10
+conda init bash
+source ~/.bashrc
+conda activate bepalice
+python3 /home/s2155435/bep1/analyse112/rfDepSampRain.py n 
