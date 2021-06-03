f07e662bcfb9f6d05cc14dd44b2da6018b09d0ee
Christie <christie@ziggo.nl>
2021-06-03 23:07:51 +0200

    new rain dataset

--- experiments/scripts/ex4-3.sh
+++ experiments/scripts/ex4-3.sh
@@ -6,12 +6,12 @@
 #SBATCH --mail-type="ALL"
 #SBATCH --partition=cpu-long
 #SBATCH -c 2
-#SBATCH --time=1-00:00:00
-#SBATCH --mem-per-cpu=32gb
+#SBATCH --time=2-00:00:00
+#SBATCH --mem-per-cpu=64gb
 
 export PYTHONPATH=/home/s2155435/bep1/
 module load Miniconda3/4.7.10
 conda init bash
 source ~/.bashrc
 conda activate bepalice
-python3 /home/s2155435/bep1/preprocess112/serialized2.py n 10 9
+python3 /home/s2155435/bep1/preprocess112/serialized2.py n 10 8
