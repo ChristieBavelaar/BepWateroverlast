467b382c535a707684edd0cd676745b5c28b271d
Christie <christie@ziggo.nl>
2021-06-07 07:38:24 +0200

    extra ex

---
+++ experiments/scripts/ex8-1l.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --job-name=ex8-1
+#SBATCH --output=/home/s2155435/ex8-1l.out
+#SBATCH --error=/home/s2155435/ex8-1l.err
+#SBATCH --mail-user="christie@ziggo.nl"
+#SBATCH --mail-type="ALL"
+#SBATCH --partition=cpu-long
+#SBATCH -c 2
+#SBATCH --time=7-00:00:00
+#SBATCH --mem-per-cpu=64gb
+
+export PYTHONPATH=/home/s2155435/bep1/
+module load Miniconda3/4.7.10
+conda init bash
+source ~/.bashrc
+conda activate bepalice
+python3 /home/s2155435/bep1/preprocess112/serialized3.py n 10 8
---
+++ experiments/scripts/ex8-2l.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --job-name=ex8-2
+#SBATCH --output=/home/s2155435/ex8-2.out
+#SBATCH --error=/home/s2155435/ex8-2.err
+#SBATCH --mail-user="christie@ziggo.nl"
+#SBATCH --mail-type="ALL"
+#SBATCH --partition=cpu-long
+#SBATCH -c 2
+#SBATCH --time=7-00:00:00
+#SBATCH --mem-per-cpu=64gb
+
+export PYTHONPATH=/home/s2155435/bep1/
+module load Miniconda3/4.7.10
+conda init bash
+source ~/.bashrc
+conda activate bepalice
+python3 /home/s2155435/bep1/preprocess112/serialized2.py n 10 8
