74e62bce5bd4547383a861f625b23b4b3432708c
Christie <christie@ziggo.nl>
2021-06-07 07:39:05 +0200

    extra ex

--- experiments/scripts/ex8-2l.sh
+++ experiments/scripts/ex8-2l.sh
@@ -7,7 +7,7 @@
 #SBATCH --partition=cpu-long
 #SBATCH -c 2
 #SBATCH --time=7-00:00:00
-#SBATCH --mem-per-cpu=64gb
+#SBATCH --mem-per-cpu=128gb
 
 export PYTHONPATH=/home/s2155435/bep1/
 module load Miniconda3/4.7.10
