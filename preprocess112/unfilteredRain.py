21d0195a7b4e090507f6450f2a77455dbb69b866
Christie <christie@ziggo.nl>
2021-06-08 12:45:12 +0200

    bugfix

--- preprocess112/unfilteredRain.py
+++ preprocess112/unfilteredRain.py
@@ -7,6 +7,8 @@ folder = '/data/s2155435/'
 pdRain = pd.read_csv(folder+'csv112/pandafied_h5_rain_2020.csv')
 data = pd.read_csv(folder+'csv112/112XY'+samplename+'.csv')
 
+pdRain = filterRain(data=pdRain, threshold=0, saveFile=folder+'csv112/rainUnFiltered'+samplename+'.csv', alice=alice)
+
 data = combineDataFrames(pd112=data, pdRain=pdRain, saveFile=folder+'csv112/112RainUnfiltered'+samplename+'.csv')
 
 posData, negData = make_labels(data=data, label_on='latitude', saveFilePos=folder+'csv112/112Labeled'+samplename+'.csv', saveFileNeg=folder+'csv112/rainLabeledUnfiltered'+samplename+'.csv')
